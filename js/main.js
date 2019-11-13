38f7cd11ff9c445f4f7a22dfd9b1db2a26b214eb
MI Shajib <mishajib222@gmail.com>
2019-11-13 17:36:17 +0600

    some code updated

--- js/main.js
+++ js/main.js
@@ -7,23 +7,46 @@ $(document).ready(function () {
 });
 
 //disable page inspection
-document.addEventListener('contextmenu', function (e) {
-    e.preventDefault();
+$( window ).on("load", function() {
+    document.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    });
+    document.onkeydown = function (e) {
+        if (e.keyCode == 123) {
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { 
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) { 
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'C'.charCodeAt(0)) { 
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'X'.charCodeAt(0)) { 
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'V'.charCodeAt(0)) { 
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'H'.charCodeAt(0)) { 
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'A'.charCodeAt(0)) { 
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)) { 
+            return false;
+        }
+    };
 });
-document.onkeydown = function (e) {
-    if (event.keyCode == 123) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-        return false;
-    }
-};
